c011363004796519a0f1ac387c858b4aa7d87bb7
Jay Stiles <JayStiles@quickenloans.com>
2018-10-26 15:48:04 -0400

    turning in

---
+++ MyPeeps_JayStiles/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPeeps
+
+{
+    class Person
+    {
+        string FirstName;
+        string LastName;
+        string EmailAddress;
+        int Age;
+        
+        
+        public Person(string firstName, string lastName, string emailAddress, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            EmailAddress = emailAddress;
+            Age = age;
+            
+        }
+
+        public virtual void Play()
+        {
+            Console.WriteLine("\nFirst name: {0} \nLast name {1} \nEmail Address: {2} \nAge: {3}", FirstName, LastName, EmailAddress, Age);
+        }
+
+    }
+}
---
+++ MyPeeps_JayStiles/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPeeps
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num =  2;
+
+        Start:
+
+            Person[] person = new Person[num];
+            person[0] = new Person("Jay", "Stiles", "JayStiles@quickenloans.com", 43);
+            person[1] = new Person("Tim", "Bro", "TimBroughton@quickenloans.com", 26);
+
+            foreach (Person character in person)
+            {
+                character.Play();
+            }
+
+            //Get userInput
+
+            Console.WriteLine( "");
+            Console.WriteLine("Enter the following requested information");
+            Console.Write("First name: ");
+            string userInputFN = Console.ReadLine();
+            
+            
+
+            Console.Write("Last name: ");
+            string userInputLN = Console.ReadLine();
+
+            Console.Write("Email address: ");
+            string userInputEA = Console.ReadLine();
+
+            Console.Write("Age: ");
+            int userInputAge = Convert.ToInt32(Console.ReadLine());
+
+            
+            if (userInputAge < 18)
+            {
+                Console.WriteLine("Cannot add minors, add someone that is over 18.");
+                goto Start;
+            }
+            else
+            {
+                num = num + 1;
+                Person[] x = new Person[num+1];
+                x[num+1] = new Person(userInputFN, userInputLN, userInputEA, userInputAge);
+                
+                goto Start;
+            }
+        }
+    }
+}
